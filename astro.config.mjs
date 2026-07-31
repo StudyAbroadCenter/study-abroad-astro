831723815bc158285898111850df9a9e7c4e93f2
Don <154335034+Steveston147@users.noreply.github.com>
2026-07-31 21:40:12 +0900

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.invalid',
+  site: 'https://studyabroadcenter.github.io',
+  base: '/study-abroad-astro',
   output: 'static',
 });
